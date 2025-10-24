b330e473168792a3d0e8aa8f82054ec09da8b387
kimywann <kimywan10@gmail.com>
2025-10-24 17:56:54 +0900

    study: useMemo로 평균 계산 최적화 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 
-function Timer() {
-  const [count, setCount] = useState<number>(0);
+const getAverage = (numbers: number[]) => {
+  console.log("평균 값을 계산 중입니다.");
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      console.log("interval 실행됨");
-      setCount((prev) => prev + 1);
-    }, 1000);
+  if (numbers.length === 0) return 0;
 
-    return () => {
-      console.log("clean up: 이전 타이머 제거됨");
-      clearInterval(id);
-    };
-  }, []);
+  const sum = numbers.reduce((acc, curr) => acc + curr);
+  return sum / numbers.length;
+};
 
-  return <div>카운트 : {count}</div>;
-}
+function App() {
+  const [list, setList] = useState<number[]>([]);
+  const [number, setNumber] = useState<string>("");
+  // input 태그에 입력된 값이기 떄문에 데이터 타입은 string
 
-export default function App() {
-  const [visible, setVisible] = useState<boolean>(false);
+  const onInsert = () => {
+    // concat: Array 인스턴스의 concat 함수는 두 개 이상의 배열을 병합하는 데 사용. 이 메서드는 기존 배열 변경하지않고, 새 배열을 반환
+    const newList = list.concat(parseInt(number));
+    setList(newList);
+    setNumber("");
+  };
+
+  const average = useMemo(() => getAverage(list), [list]);
 
   return (
     <div>
-      {visible && <Timer />}
-      <button onClick={() => setVisible(!visible)}>
-        {visible ? "숨기기" : "보이기"}
-      </button>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={onInsert}>등록</button>
+
+      <ul>
+        {list.map((item: number, index: number) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <div>
+        <b>평균 값: {average}</b>
+      </div>
     </div>
   );
 }
+
+export default App;
