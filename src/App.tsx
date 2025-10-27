3c8dd76a0945d8b1744e664c1c7a8d2a86eadea3
kimywann <kimywan10@gmail.com>
2025-10-27 19:08:41 +0900

    study: useCallback으로 함수 메모이제이션 및 클로저 문제 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const getAverage = (numbers: number[]) => {
   console.log("평균 값을 계산 중입니다.");
@@ -10,26 +10,40 @@ const getAverage = (numbers: number[]) => {
 };
 
 function App() {
+  // useCallback은 useMemo와 비슷한 함수. 주로 렌더링 성능 최적화 하는 상황에서 사용
+  // 이 훅은 만들어 놓았던 함수를 재사용 할 수 있다.
+
+  // useCallback의 첫 번째 파라미터에는 생성하고 싶은 함수를 전달하고,
+  // 두 번째 파라미터에는 배열을 넣으면 된다.
+
+  // onChange 처럼 비어있는 배열을 넣게 되면 컴포넌트가 렌더링 될 때, 만들었던 함수를 계혹해서 재사용하게 되며
+  // onInsert 처럼 배열 안에 number와 list를 넣게 되면, 인풋 내용이 바뀌거나 새로운 항목이 추가 되었을 떼, 새로 만들어진 함수를 사용하게 됨.
+
   const [list, setList] = useState<number[]>([]);
   const [number, setNumber] = useState<string>("");
   // input 태그에 입력된 값이기 떄문에 데이터 타입은 string
 
-  const onInsert = () => {
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(event.target.value);
+  }, []); // 컴포넌트가 처음 렌더링 될 때만 함수를 생성한다.
+
+  const onInsert = useCallback(() => {
     // concat: Array 인스턴스의 concat 함수는 두 개 이상의 배열을 병합하는 데 사용. 이 메서드는 기존 배열 변경하지않고, 새 배열을 반환
     const newList = list.concat(parseInt(number));
     setList(newList);
-    setNumber("");
-  };
+    setNumber(""); // number 상태 값 초기화
+  }, [number, list]);
+
+  // useCallback은 첫 렌더링 때 한 번만 함수 onInsert를 생성한다. ([])
+  // onInsert 안에서 사용하는 list, number는 초기값의 복사본으로 함수안에 닫혀 (closed over) 있다.
+  // 이후 number나 list가 변경되어도, onInsert는 옛날 값을 계속 사용한다.
+  // 이게 클로저(closure) 문제이다.
 
   const average = useMemo(() => getAverage(list), [list]);
 
   return (
     <div>
-      <input
-        type="text"
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
-      />
+      <input type="text" value={number} onChange={onChange} />
       <button onClick={onInsert}>등록</button>
 
       <ul>
