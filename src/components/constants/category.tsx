068f9d705cc8a81be7facb854aa32314a975dbbc
kimywann <kimywan10@gmail.com>
2025-10-28 23:20:32 +0900

    chore: 토픽 카테고리 상수(TOPIC_CATEGORY) 정의

--- src/components/constants/category.tsx
+++ src/components/constants/category.tsx
@@ -5,6 +5,7 @@ import {
   Router,
   Footprints,
   Goal,
+  CircleEllipsis,
 } from "lucide-react";
 
 export const SIDEBAR_CATEGORY = [
@@ -43,5 +44,44 @@ export const SIDEBAR_CATEGORY = [
     lable: "취업",
     category: "job",
     icon: <Goal />,
+  },
+  {
+    id: 7,
+    lable: "기타",
+    category: "etc",
+    icon: <CircleEllipsis />,
+  },
+];
+
+export const TOPIC_CATEGORY = [
+  {
+    id: 1,
+    lable: "프론트엔드",
+    category: "front-end",
+  },
+  {
+    id: 2,
+    lable: "백엔드",
+    category: "back-end",
+  },
+  {
+    id: 3,
+    lable: "인프라",
+    category: "infrastructure",
+  },
+  {
+    id: 4,
+    lable: "자기계발",
+    category: "self-development",
+  },
+  {
+    id: 5,
+    lable: "취업",
+    category: "job",
+  },
+  {
+    id: 6,
+    lable: "기타",
+    category: "etc",
   },
 ];
