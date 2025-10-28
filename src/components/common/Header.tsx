6ba00fdb3d96b8f43aa3caa33f9fc2f00cf8ae55
kimywann <kimywan10@gmail.com>
2025-10-28 18:10:13 +0900

    style: 헤더 배경색 추가, 로고 이미지 크기 h-10 w-10로 조정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,14 +2,14 @@ import { Separator } from "../ui";
 
 function Header() {
   return (
-    <header className="fixed top-0 z-20 flex w-full items-center justify-center">
-      <div className="flex w-full max-w-[1328px] items-center justify-between px-6 py-3">
+    <header className="bg-background fixed top-0 z-20 flex w-full items-center justify-center">
+      <div className="flex w-full max-w-[1328px] items-center justify-between px-6 py-2">
         {/* 로고 & 내비게이션 메뉴 UI */}
         <div className="itmes-center flex gap-5">
           <img
             src="/logo.svg"
             alt="logo"
-            className="h-12 w-12 cursor-pointer"
+            className="h-10 w-10 cursor-pointer"
           />
           <div className="flex items-center gap-5">
             <div className="cursor-pointer font-semibold hover:text-blue-500">
