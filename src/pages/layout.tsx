0272b53ead53735deab8ebd8e28ccaaf5509dae8
kimywann <kimywan10@gmail.com>
2025-10-28 23:15:15 +0900

    style: 루트 레이아웃 Outlet 부분 mt 속성 지정

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
   return (
     <div className="page">
       <Header />
-      <div className="container">
+      <div className="container mt-2">
         <Outlet />
       </div>
       <Footer />
