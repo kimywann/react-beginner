4499747bceeee3de7b107c1d7770a9bc98d999da
kimywann <kimywan10@gmail.com>
2025-10-28 16:41:54 +0900

    feat: App 레이아웃에 사이드바 통합

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header, Footer } from "./components/common";
+import { Sidebar } from "./components/common/Sidebar";
 import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
@@ -6,7 +7,14 @@ function App() {
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <div className="page">
         <Header />
-        <div className="container"></div>
+        <div className="container">
+          <main className="flex h-full min-h-[720px] w-full gap-6 p-6">
+            {/* 카테고리 사이드바 */}
+            <Sidebar />
+            {/* 토픽 콘텐츠 */}
+            <section></section>
+          </main>
+        </div>
         <Footer />
       </div>
     </ThemeProvider>
