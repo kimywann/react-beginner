8a1030097066ff3b47e0ae650d4acacb4c47bbad
kimywann <kimywan10@gmail.com>
2025-10-28 20:20:32 +0900

    refactor: App.tsx를 pages 폴더로 이동하고 index.tsx로 변경

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { Sidebar } from "../components/common/Sidebar";
+import { Button } from "../components/ui";
+import { PencilLine } from "lucide-react";
+
+function App() {
+  return (
+    <main className="flex h-full min-h-[720px] w-full gap-6 p-6">
+      <div className="fixed right-1/2 bottom-10 z-20 translate-x-1/2 items-center">
+        <Button
+          variant={"destructive"}
+          className="rounded-full !bg-blue-500 !px-6 !py-5"
+        >
+          <PencilLine />
+          나만의 토픽 작성
+        </Button>
+      </div>
+      {/* 카테고리 사이드바 */}
+      <Sidebar />
+      {/* 토픽 콘텐츠 */}
+      <section></section>
+    </main>
+  );
+}
+
+export default App;
