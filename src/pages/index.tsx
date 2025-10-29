a4b2c91cdaeb00ffc5edf60284e3140d4f128e6e
kimywann <kimywan10@gmail.com>
2025-10-29 14:16:10 +0900

    feat: 로그인 상태에 따라 토픽 작성 페이지 또는 로그인 페이지로 라우팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,27 @@ import { Sidebar } from "../components/common/Sidebar";
 import { Button } from "../components/ui";
 import { PencilLine } from "lucide-react";
 
+import { useAuthStore } from "@/stores";
+
 function App() {
+  const user = useAuthStore((state) => state.user);
   const navigate = useNavigate();
+
+  const handleRoute = () => {
+    if (user?.id || user?.email || user?.role) {
+      navigate("/topic/write");
+    } else {
+      navigate("/sign-in");
+    }
+  };
+
   return (
     <main className="flex h-full min-h-[720px] w-full gap-6 p-6">
       <div className="fixed right-1/2 bottom-10 z-20 translate-x-1/2 items-center">
         <Button
           variant={"destructive"}
           className="rounded-full !bg-blue-500 !px-6 !py-5"
-          onClick={() => navigate("/topic/write")}
+          onClick={handleRoute}
         >
           <PencilLine />
           나만의 토픽 작성
