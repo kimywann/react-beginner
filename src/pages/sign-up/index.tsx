313c6141215a635196e66330ac2ad2be0bc3bdbe
kimywann <kimywan10@gmail.com>
2025-10-29 11:12:27 +0900

    feat: 회원가입 폼 및 기능 구현

---
+++ src/pages/sign-up/index.tsx
@@ -0,0 +1,179 @@
+import supabase from "@/lib/supabase";
+
+import { NavLink, useNavigate } from "react-router";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+} from "@/components/ui";
+import { toast } from "sonner";
+
+const formSchema = z
+  .object({
+    email: z.email({
+      error: "올바른 형식의 이메일 주소를 입력해주세요.",
+    }),
+    password: z.string().min(8, {
+      error: "비밀번호는 최소 8자 이상이어야 합니다.",
+    }),
+    confirmPassword: z.string().min(8, {
+      error: "비밀번호 확인을 입력해주세요.",
+    }),
+  })
+  .superRefine(({ password, confirmPassword }, ctx) => {
+    if (password !== confirmPassword) {
+      ctx.addIssue({
+        code: "custom",
+        message: "비밀번호가 일치하지 않습니다.",
+        path: ["confirmPassword"],
+      });
+    }
+  });
+
+export default function SignUp() {
+  const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const {
+        data: { user, session },
+        error,
+      } = await supabase.auth.signUp({
+        email: values.email,
+        password: values.password,
+      });
+
+      if (error) {
+        toast.error(error.message);
+        return;
+      }
+
+      if (user && session) {
+        const { data, error } = await supabase
+          .from("user")
+          .insert({
+            id: user.id,
+          })
+          .select();
+
+        if (data) {
+          toast.success("회원가입이 완료되었습니다.");
+          navigate("/sign-in");
+        }
+
+        if (error) {
+          console.error(error);
+          toast.error(error.message);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error(`${error}`);
+    }
+  };
+
+  return (
+    <main className="flex h-full min-h-[720px] w-full items-center justify-center gap-6 p-6">
+      <div className="flex w-100 max-w-100 flex-col gap-6 px-6">
+        <div className="flex flex-col">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            회원가입
+          </h4>
+          <p className="text-muted-foreground">
+            회원가입을 위한 정보를 입력해주세요.
+          </p>
+        </div>
+        <div className="grid gap-3">
+          {/* 회원가입 폼 */}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>이메일</FormLabel>
+                    <FormControl>
+                      <Input placeholder="이메일을 입력하세요." {...field} />
+                    </FormControl>
+                    <FormMessage className="text-xs" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>비밀번호 (8자 이상)</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="비밀번호를 입력하세요."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-xs" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>비밀번호 확인</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="비밀번호 확인을 입력하세요."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-xs" />
+                  </FormItem>
+                )}
+              />
+              <div className="flex w-full flex-col gap-3">
+                <div className="flex items-center">
+                  <Button
+                    type="submit"
+                    variant={"outline"}
+                    className="flex-1 !bg-blue-400 !text-white"
+                  >
+                    회원가입
+                  </Button>
+                </div>
+                <div className="text-center">
+                  이미 계정이 있으신가요?
+                  <NavLink to={"/sign-in"} className="ml-1 underline">
+                    로그인
+                  </NavLink>
+                </div>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </main>
+  );
+}
