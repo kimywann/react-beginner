43b19543f56e190ab4762c48a60295e906713099
kimywann <kimywan10@gmail.com>
2025-10-31 15:36:13 +0900

    chore: 직업 옵션 상수 추가

--- src/components/constants/profile.tsx
+++ src/components/constants/profile.tsx
@@ -1,5 +1,27 @@
-export const SORT_OPTIONS = ["최신순", "인기순"] as const;
-export const JOB_ROLE_OPTIONS = [
+export const JOB_OPTIONS = [
+  {
+    id: 1,
+    lable: "고등학생",
+  },
+  {
+    id: 2,
+    lable: "대학생",
+  },
+  {
+    id: 3,
+    lable: "취업준비생",
+  },
+  {
+    id: 4,
+    lable: "인턴",
+  },
+  {
+    id: 5,
+    lable: "직장인",
+  },
+] as const;
+
+export const POSITION_ROLE_OPTIONS = [
   {
     id: 1,
     lable: "프론트엔드",
@@ -25,6 +47,7 @@ export const JOB_ROLE_OPTIONS = [
     lable: "기타",
   },
 ] as const;
+
 export const EXPERIENCE_OPTIONS = [
   {
     id: 1,
@@ -51,6 +74,7 @@ export const EXPERIENCE_OPTIONS = [
     lable: "Lead (10년 이상)",
   },
 ] as const;
+
 export const REGION_OPTIONS = [
   {
     id: 1,
@@ -58,7 +82,7 @@ export const REGION_OPTIONS = [
   },
   {
     id: 2,
-    lable: "서울특별시",
+    lable: "서울",
   },
   {
     id: 3,
@@ -66,31 +90,31 @@ export const REGION_OPTIONS = [
   },
   {
     id: 4,
-    lable: "부산광역시",
+    lable: "부산",
   },
   {
     id: 5,
-    lable: "인천광역시",
+    lable: "인천",
   },
   {
     id: 6,
-    lable: "대구광역시",
+    lable: "대구",
   },
   {
     id: 7,
-    lable: "광주광역시",
+    lable: "광주",
   },
   {
     id: 9,
-    lable: "대전광역시",
+    lable: "대전",
   },
   {
     id: 10,
-    lable: "울산광역시",
+    lable: "울산",
   },
   {
     id: 11,
-    lable: "강원특별자치도",
+    lable: "강원도",
   },
   {
     id: 12,
@@ -115,5 +139,9 @@ export const REGION_OPTIONS = [
   {
     id: 17,
     lable: "경상남도",
+  },
+  {
+    id: 18,
+    lable: "제주도",
   },
 ] as const;
