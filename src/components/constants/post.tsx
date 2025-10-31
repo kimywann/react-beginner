eff76f8ebdc0eb540711e0cf9b1788a2022235d7
kimywann <kimywan10@gmail.com>
2025-11-01 03:49:21 +0900

    chore: post 상수, 타입 재정의

---
+++ src/components/constants/post.tsx
@@ -0,0 +1,244 @@
+export const POST_CATEGORY = [
+  {
+    id: 1,
+    lable: "프로젝트",
+  },
+  {
+    id: 2,
+    lable: "스터디",
+  },
+];
+
+export const POST_MEMBERS = [
+  {
+    id: 1,
+    lable: "미정",
+  },
+  {
+    id: 2,
+    lable: "1명",
+  },
+  {
+    id: 3,
+    lable: "2명",
+  },
+  {
+    id: 4,
+    lable: "3명",
+  },
+  {
+    id: 5,
+    lable: "4명",
+  },
+  {
+    id: 6,
+    lable: "5명",
+  },
+  {
+    id: 7,
+    lable: "6명",
+  },
+  {
+    id: 8,
+    lable: "7명",
+  },
+  {
+    id: 9,
+    lable: "8명",
+  },
+  {
+    id: 10,
+    lable: "9명 이상",
+  },
+];
+
+export const POST_PROGRESS_METHOD = [
+  {
+    id: 1,
+    lable: "온라인",
+  },
+  {
+    id: 2,
+    lable: "오프라인",
+  },
+  {
+    id: 3,
+    lable: "온/오프라인",
+  },
+];
+
+export const POST_CONTACT = [
+  {
+    id: 1,
+    lable: "오픈채팅",
+  },
+  {
+    id: 2,
+    lable: "이메일",
+  },
+];
+
+export const POST_DURATION = [
+  {
+    id: 1,
+    lable: "미정",
+  },
+  {
+    id: 2,
+    lable: "1주일",
+  },
+  {
+    id: 3,
+    lable: "2주일",
+  },
+  {
+    id: 4,
+    lable: "3주일",
+  },
+  {
+    id: 5,
+    lable: "1개월",
+  },
+  {
+    id: 6,
+    lable: "2개월",
+  },
+  {
+    id: 7,
+    lable: "3개월",
+  },
+  {
+    id: 8,
+    lable: "장기",
+  },
+];
+
+export const POST_ROLES = [
+  {
+    id: 1,
+    lable: "전체",
+  },
+  {
+    id: 2,
+    lable: "프론트엔드",
+  },
+  {
+    id: 3,
+    lable: "백엔드",
+  },
+  {
+    id: 4,
+    lable: "풀스택",
+  },
+  {
+    id: 5,
+    lable: "기획자",
+  },
+  {
+    id: 6,
+    lable: "디자이너",
+  },
+  {
+    id: 7,
+    lable: "AI",
+  },
+  {
+    id: 8,
+    lable: "데이터",
+  },
+  {
+    id: 9,
+    lable: "안드로이드",
+  },
+  {
+    id: 10,
+    lable: "iOS",
+  },
+  {
+    id: 11,
+    lable: "데브옵스",
+  },
+  {
+    id: 12,
+    lable: "PM",
+  },
+  {
+    id: 13,
+    lable: "마케터",
+  },
+];
+
+export const POST_TECH_STACK = [
+  {
+    id: 1,
+    lable: "JavaScript",
+  },
+  {
+    id: 2,
+    lable: "TypeScript",
+  },
+  {
+    id: 3,
+    lable: "React",
+  },
+  {
+    id: 4,
+    lable: "Vue",
+  },
+  {
+    id: 5,
+    lable: "Next.js",
+  },
+  {
+    id: 5,
+    lable: "Java",
+  },
+  {
+    id: 6,
+    lable: "Kotlin",
+  },
+  {
+    id: 7,
+    lable: "Python",
+  },
+  {
+    id: 8,
+    lable: "Swift",
+  },
+  {
+    id: 9,
+    lable: "C#",
+  },
+  {
+    id: 10,
+    lable: "C++",
+  },
+  {
+    id: 15,
+    lable: "Figma",
+  },
+  {
+    id: 16,
+    lable: "Git",
+  },
+  {
+    id: 17,
+    lable: "Flutter",
+  },
+  {
+    id: 18,
+    lable: "Docker",
+  },
+  {
+    id: 19,
+    lable: "Kubernetes",
+  },
+  {
+    id: 20,
+    lable: "AWS",
+  },
+  {
+    id: 21,
+    lable: "React Native",
+  },
+];
--- src/types/post.type.ts
+++ src/types/post.type.ts
@@ -5,10 +5,17 @@ export enum POST_STATUS {
 
 export interface POST {
   id: number;
-  created_at: Date | string;
   author: any; // 추후 변경
   title: string;
   content: string;
-  category: string;
+  created_at: Date | string;
   status: POST_STATUS;
+
+  category: string;
+  progress_method: string; // 진행 방식
+  members: number | string; // 모집 인원
+  contact: string; // 연락 방법
+  contact_url: string; // 연락 방법 URL
+  duration: string; // 진행 기간
+  recruitment_deadline: Date | string; // 모집 마감일
 }
