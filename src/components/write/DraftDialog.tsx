517c86366d199dacd6d7568de5e14b71e862d0af
kimywann <kimywan10@gmail.com>
2025-11-01 03:48:49 +0900

    fix: 임시 저장 글 라우트 경로 개선

--- src/components/write/DraftDialog.tsx
+++ src/components/write/DraftDialog.tsx
@@ -81,7 +81,7 @@ export function DraftDialog({ children }: Props) {
                   <div
                     key={draft.id}
                     className="bg-card/50 flex h-14 w-full cursor-pointer items-center justify-between gap-3 rounded-md border px-4 py-2"
-                    onClick={() => navigate(`/post/${draft.id}/write`)}
+                    onClick={() => navigate(`/posts/${draft.id}/edit`)}
                   >
                     <div className="flex items-start gap-2">
                       <Badge className="text-foreground aspect-square h-6 w-6 rounded-sm bg-green-200">
