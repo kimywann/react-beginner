1ebd1ca3fad0740c809788045403686239d33c36
kimywann <kimywan10@gmail.com>
2025-11-05 10:16:23 +0900

    feat: 모바일 햄버거 메뉴 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,12 +2,23 @@ import supabase from "@/lib/supabase";
 import { useAuthStore } from "@/stores";
 import { NavLink, useNavigate } from "react-router";
 
-import { Button, Separator } from "@/components/ui";
+import {
+  Button,
+  Separator,
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui";
 import { DraftDialog } from "@/components/write";
-import { Archive } from "lucide-react";
+import { Archive, Menu } from "lucide-react";
 import { toast } from "sonner";
+import { useState } from "react";
 
 function Header() {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
   const navigate = useNavigate();
 
   const user = useAuthStore((state) => state.user);
@@ -25,6 +36,10 @@ function Header() {
   };
 
   const handleRoute = async () => {
+    if (isHamburgerOpen) {
+      setIsHamburgerOpen(false);
+    }
+
     if (user?.id || user?.email || user?.role) {
       const { data, error } = await supabase
         .from("post")
@@ -64,7 +79,8 @@ function Header() {
             className="h-12 w-12 cursor-pointer"
             onClick={() => navigate("/")}
           />
-          <div className="flex items-center gap-10">
+
+          <div className="hidden items-center gap-10 md:flex">
             <NavLink
               to="/"
               className="cursor-pointer font-semibold hover:text-blue-500"
@@ -80,42 +96,116 @@ function Header() {
           </div>
         </div>
 
-        <div className="flex gap-5">
-          <div className="flex gap-2">
-            <Button
-              variant={"destructive"}
-              className="rounded-xl !bg-blue-500 !px-5 !py-5"
-              onClick={handleRoute}
-            >
-              모집 글 작성
+        {/* 오른쪽 영역: 로그인/로그아웃 + 버튼들 */}
+        <div className="hidden items-center gap-2 md:flex">
+          {/* 버튼들 */}
+          <Button
+            variant={"destructive"}
+            className="rounded-xl !bg-blue-500 !px-5 !py-5"
+            onClick={handleRoute}
+          >
+            모집 글 작성
+          </Button>
+          <DraftDialog>
+            <Button variant={"outline"} className="h-10 w-10 rounded-xl">
+              <Archive />
             </Button>
-            <DraftDialog>
-              <div className="relative">
-                <Button variant={"outline"} className="r h-10 w-10 rounded-xl">
-                  <Archive />
-                </Button>
-                {/* <CircleSmall
-                  size={14}
-                  className="absolute top-0 right-0 text-blue-500"
-                  fill="#1976D2"
-                /> */}
-              </div>
-            </DraftDialog>
-          </div>
+          </DraftDialog>
+
+          <Separator orientation="vertical" className="!h-4" />
 
           {user ? (
-            <div className="flex items-center gap-2">
+            <>
               <span>{user.email}</span>
               <Separator orientation="vertical" className="!h-4" />
               <span onClick={handleLogout} className="cursor-pointer">
                 로그아웃
               </span>
-            </div>
+            </>
           ) : (
-            <div className="flex items-center gap-2">
-              <NavLink to="/sign-in">로그인</NavLink>
-            </div>
+            <NavLink to="/sign-in">로그인</NavLink>
           )}
+        </div>
+
+        <div className="md:hidden">
+          <Sheet open={isHamburgerOpen} onOpenChange={setIsHamburgerOpen}>
+            <SheetTrigger asChild>
+              <Button variant="ghost">
+                <Menu className="size-6" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                {user ? (
+                  <SheetTitle className="text-lg">{user?.email}</SheetTitle>
+                ) : (
+                  <SheetTitle className="text-muted-foreground text-lg">
+                    로그인이 필요합니다.
+                  </SheetTitle>
+                )}
+              </SheetHeader>
+
+              <Separator />
+
+              <nav className="flex flex-col gap-2 p-4">
+                <NavLink
+                  to="/"
+                  className="cursor-pointer rounded-md p-2 text-lg font-semibold duration-300 hover:bg-gray-100"
+                  onClick={() => {
+                    setIsHamburgerOpen(false);
+                  }}
+                >
+                  팀원 모집
+                </NavLink>
+                <NavLink
+                  to="/recruits"
+                  className="cursor-pointer rounded-md p-2 text-lg font-semibold duration-300 hover:bg-gray-100"
+                  onClick={() => {
+                    setIsHamburgerOpen(false);
+                  }}
+                >
+                  동료 찾기
+                </NavLink>
+                <Button
+                  variant="ghost"
+                  className="h-11 cursor-pointer justify-start rounded-md p-2 text-lg font-semibold duration-300 hover:bg-gray-100"
+                  onClick={handleRoute}
+                >
+                  모집 글 작성
+                </Button>
+                <DraftDialog>
+                  <Button
+                    variant="ghost"
+                    className="h-11 cursor-pointer justify-start rounded-md p-2 text-lg font-semibold duration-300 hover:bg-gray-100"
+                    onClick={() => {
+                      navigate("/drafts");
+                      setIsHamburgerOpen(false);
+                    }}
+                  >
+                    임시 저장 목록
+                  </Button>
+                </DraftDialog>
+              </nav>
+              <SheetFooter>
+                {user ? (
+                  <Button type="submit" onClick={handleLogout}>
+                    로그아웃
+                  </Button>
+                ) : (
+                  <Button
+                    type="button"
+                    onClick={() => {
+                      navigate("/sign-in");
+                      setIsHamburgerOpen(false);
+                    }}
+                    className="w-full border-1 border-gray-300"
+                  >
+                    로그인
+                  </Button>
+                )}
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </header>
