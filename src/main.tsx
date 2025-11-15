40098a0ab777f1c06c338f5c878a1e2d8a930574
kimywann <kimywan10@gmail.com>
2025-11-16 00:13:49 +0900

    chore: React Query Devtools 설치 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -15,12 +15,14 @@ import FindTeammates from "./pages/find-teammates/index.tsx";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <BrowserRouter>
           <Routes>
