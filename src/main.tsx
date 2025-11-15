15dead0eb050524c28c57b05651327cac4b56039
kimywann <kimywan10@gmail.com>
2025-11-15 22:36:08 +0900

    feat: @tanstack/react-query 패키지 추가 및 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -13,24 +13,30 @@ import PostDetail from "./pages/post/[id]/detail.tsx";
 import Recruit from "./pages/recruit/index.tsx";
 import FindTeammates from "./pages/find-teammates/index.tsx";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
+
 import "./index.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route index element={<App />} />
-            <Route path="sign-up" element={<SignUp />} />
-            <Route path="sign-in" element={<SignIn />} />
-            <Route path="recruit" element={<Recruit />} />
-            <Route path="recruit/posts/:id" element={<PostDetail />} />
-            <Route path="recruit/posts/:id/edit" element={<PostCreate />} />
-            <Route path="find-teammates" element={<FindTeammates />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <Toaster richColors position="top-center" />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route index element={<App />} />
+              <Route path="sign-up" element={<SignUp />} />
+              <Route path="sign-in" element={<SignIn />} />
+              <Route path="recruit" element={<Recruit />} />
+              <Route path="recruit/posts/:id" element={<PostDetail />} />
+              <Route path="recruit/posts/:id/edit" element={<PostCreate />} />
+              <Route path="find-teammates" element={<FindTeammates />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <Toaster richColors position="top-center" />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
